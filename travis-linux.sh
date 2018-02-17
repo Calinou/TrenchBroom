ba53dd08beb6e05375f2bd93de77f8ef8117107a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-02-17 13:20:46 +0100

    Add AppImage builds to Travis CI
    This makes a portable Linux build available. For more information, see
    <https://appimage.org/>.

--- travis-linux.sh
+++ travis-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -o verbose
+set -euo pipefail
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
@@ -11,24 +11,40 @@ sudo apt-get -y install libgtk2.0-dev freeglut3 freeglut3-dev libglew-dev mesa-c
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.7z
 if [[ "daf03ed0006e41334f10ceeb3aa2d20c63aacd42" != $(sha1sum wxWidgets-3.1.0.7z | cut -f1 -d' ') ]] ; then exit 1 ; fi
 7z x -o"wxWidgets" -y wxWidgets-3.1.0.7z > /dev/null
-cd wxWidgets || exit 1
-patch -p0 < ../patches/wxWidgets/*.patch || exit 1
+cd wxWidgets
+patch -p0 < ../patches/wxWidgets/*.patch
 mkdir build-release
 cd build-release
-CC=gcc-5 CXX=g++-5 ../configure --quiet --disable-shared --with-opengl --with-cxx=14 --with-gtk=2 --prefix=$(pwd)/install --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
+CC=gcc-5 CXX=g++-5 ../configure --quiet --disable-shared --with-opengl --with-cxx=14 --with-gtk=2 --prefix="$(pwd)/install" --disable-precomp-headers --with-libpng=builtin --with-libtiff=builtin --with-libjpeg=builtin && make -j2 && make install
 cd ..
 cd ..
 
-# Build TB
+# Build TrenchBroom
 
 mkdir build
 cd build
-CC=gcc-5 CXX=g++-5 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-release/install || exit 1
-cmake --build . --config Release || exit 1
-cpack || exit 1
+CC=gcc-5 CXX=g++-5 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX="$(pwd)/../wxWidgets/build-release/install"
+cmake --build . --config Release
+cpack
 
 ./generate_checksum_deb.sh
 ./generate_checksum_rpm.sh
+
+# Create AppImage for Linux
+
+mkdir -p appdir/usr/bin appdir/usr/share/icons
+strip trenchbroom
+cp trenchbroom appdir/usr/bin/trenchbroom
+cp ../app/resources/linux/trenchbroom.desktop appdir/trenchbroom.desktop
+cp ../app/resources/linux/icons/icon_256.png appdir/usr/share/icons/trenchbroom.png
+
+wget -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod +x "linuxdeployqt-continuous-x86_64.AppImage"
+./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
+rm -f "linuxdeployqt-continuous-x86_64.AppImage"
+./squashfs-root/AppRun "appdir/trenchbroom.desktop" -appimage
+# Move the AppImage to the build artifacts directory
+cp *.AppImage ..
 
 # Run tests (wxgtk needs an X server running for the app to initialize)
 
